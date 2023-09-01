404bc87bc551b99686ac168098dfba0815149c20
hejazy <hegazy@outlook.com>
2023-09-01 19:53:11 +0300

    UPD: add the needed missing companants

---
+++ src/components/previewer/previewer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './previewer.scss';
+
+export function Privewer({
+  images = [],
+}) {
+  return (
+    <div className="previewer-class">
+      <canvas/>
+   </div>
+
+  );
+}
+
+
+
